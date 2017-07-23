f85be412c0676f2dd4d1b157e385b289d4590287
Olivier Riffaud <riffaud.o+github@gmail.com>
2017-07-23 21:44:38 +1000

    Use common handler for setting the content type

--- index.js
+++ index.js
@@ -8,12 +8,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use((error, req, res, next) => {
     if (error instanceof SyntaxError) {
         res.status(400);
-        res.setHeader('Content-Type', 'application/json');
         res.send(JSON.stringify({ error: "error parsing JSON" }));
     } else {
         next();
     }
 });
+
+app.use((req, res, next) => {
+    res.setHeader('Content-Type', 'application/json');
+    next();
+});
+
 
 app.post('/flights', (req, res) => {
     var flights = req.body.flights;
